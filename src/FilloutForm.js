8d3fdf976bd0206fca6069b010c2e8899d1f7a90
Ilya Katz <ilyakatz@gmail.com>
2023-10-07 19:24:05 -0700

    Do not use jquery

--- src/FilloutForm.js
+++ src/FilloutForm.js
@@ -44,56 +44,162 @@ export function fillOutForm(
 
 function updateTypeOfStay(typeOfStay) {
   console.log(`Updating type of stay`);
-
-  $formJquery("select")[0].value = typeOfStay;
-  if ($formJquery("select")[0].value === typeOfStay) {
-    console.log(`✅ Type of stay set to  ${$formJquery("select")[0].value}`);
+  const selectElement = specificFrame.querySelector("select");
+  selectElement.value = typeOfStay;
+  if (selectElement.value === typeOfStay) {
+    console.log(`✅ Type of stay set to ${selectElement.value}`);
   } else {
     console.log(`❌ Type of stay is not set correctly`);
   }
 }
 
 function updateAirbnbListingId(airbnbId) {
-  var $listingId = $formJquery('label:contains("Listing ID:")');
-  var $listingIdInput = $listingId.parents(".ACA_Form").find("input");
-  $listingIdInput.val(airbnbId);
-  if ($listingIdInput.val() === airbnbId) {
-    console.log(`✅ listing id set to ${airbnbId}`);
+  console.log(`Updating Airbnb Listing ID`);
+
+  // Find the label element
+  var labels = specificFrame.querySelectorAll("label");
+  var labelElement = null;
+  labels.forEach(function (label) {
+    if (label.innerText === "Listing ID:") {
+      labelElement = label;
+    }
+  });
+
+  if (labelElement) {
+    // Find the input element
+    var input = labelElement.closest(".ACA_Form").querySelector("input");
+
+    if (input) {
+      input.value = airbnbId;
+    } else {
+      console.log(`❌ Input element not found for ${input}`);
+    }
   } else {
-    console.log(`❌ listing id is not set correctly`);
+    console.log(`❌ Label element not found for ${labelElement}`);
   }
 }
 
 function selectAirbnb() {
-  var serviceLabel = $formJquery('label:contains("Service Name:")');
-  var serviceSelect = serviceLabel.parents(".ACA_Form").find("select");
-  var serviceDropdownValues = [];
-  $formJquery(serviceSelect)
-    .find("option")
-    .each(function () {
-      var value = $formJquery(this).val();
-      if (value !== "") {
-        serviceDropdownValues.push(value);
+  console.log(`Selecting Airbnb service`);
+
+  // Find the label element
+  var labels = specificFrame.querySelectorAll("label");
+  var serviceLabel = null;
+  var regex = /^Service Name/;
+  labels.forEach(function (label) {
+    if (regex.test(label.textContent)) {
+      serviceLabel = label;
+    }
+  });
+
+  if (serviceLabel) {
+    // Find the parent element with class "ACA_Form"
+    var parentACAForm = serviceLabel.closest(".ACA_Form");
+
+    if (parentACAForm) {
+      // Find the select element within the parent element
+      var serviceSelect = parentACAForm.querySelector("select");
+
+      if (serviceSelect) {
+        // Create an array for storing select options values
+        var serviceDropdownValues = [];
+
+        // Iterate through the option elements
+        serviceSelect.querySelectorAll("option").forEach(function (option) {
+          var value = option.value;
+          if (value !== "") {
+            serviceDropdownValues.push(value);
+          }
+        });
+
+        if (serviceDropdownValues.length > 0) {
+          var service = serviceDropdownValues[0];
+
+          // Set the value of the select element
+          serviceSelect.value = service;
+          console.log(`✅ Service set to ${service}`);
+        } else {
+          console.log(`❌ No valid service values found`);
+        }
+      } else {
+        console.log(`❌ Select element not found within .ACA_Form`);
       }
-    });
-  var service = serviceDropdownValues[0];
-  serviceSelect.val(service);
-  console.log(`✅ service set to  ${service}`);
+    } else {
+      console.log(`❌ .ACA_Form parent element not found`);
+    }
+  } else {
+    console.log(`❌ Label element not found`);
+  }
 }
 
 function updateCheckoutDate(checkOutDate) {
-  var checkoutDate = $formJquery('label:contains("Check Out Date:")');
-  checkoutDate.parents(".ACA_Form").find("input").val(checkOutDate);
+  if (updateDate("Check Out Date:", checkOutDate)) {
+    console.log(`✅ Check Out Date updated successfully`);
+  } else {
+    console.log(`❌ Check Out Date update failed`);
+  }
 }
 
 function updateCheckinDate(checkInDate) {
-  var checkinDate = $formJquery('label:contains("Check In Date:")');
-  checkinDate.parents(".ACA_Form").find("input").val(checkInDate);
+  if (updateDate("Check In Date:", checkInDate)) {
+    console.log(`✅ Check In Date updated successfully`);
+  } else {
+    console.log(`❌ Check In Date update failed`);
+  }
+}
+
+function updateDate(labelText, dateValue) {
+  // Find the label element
+  var labels = specificFrame.querySelectorAll("label");
+  var dateLabel = null;
+  labels.forEach(function (label) {
+    if (label.textContent.includes(labelText)) {
+      dateLabel = label;
+    }
+  });
+
+  if (dateLabel) {
+    // Find the input element
+    var dateInput = dateLabel.closest(".ACA_Form").querySelector("input");
+
+    if (dateInput) {
+      // Set the value of the input element
+      dateInput.value = dateValue;
+      return true; // Operation succeeded
+    } else {
+      console.log(`❌ Input element not found for ${labelText}`);
+    }
+  } else {
+    console.log(`❌ Label element not found for ${labelText}`);
+  }
+  return false; // Operation failed
 }
 
 function setCurrentYear(currentYear) {
-  var reportingYearLabel = $formJquery('label:contains("Reporting Year:")');
-  reportingYearLabel.parents(".ACA_Form").find("input").val(currentYear);
+  // Find the label element
+  var labels = specificFrame.querySelectorAll("label");
+  var reportingYearLabel = null;
+  labels.forEach(function (label) {
+    if (label.textContent.includes("Reporting Year:")) {
+      reportingYearLabel = label;
+    }
+  });
+
+  if (reportingYearLabel) {
+    // Find the input element
+    var reportingYearInput = reportingYearLabel
+      .closest(".ACA_Form")
+      .querySelector("input");
+
+    if (reportingYearInput) {
+      // Set the value of the input element
+      reportingYearInput.value = currentYear;
+    } else {
+      console.log(`❌ Input element not found`);
+    }
+  } else {
+    console.log(`❌ Label element not found`);
+  }
 }
 
 function updateCurrentQuarter(currentQuarter) {
@@ -106,30 +212,75 @@ function updateCurrentQuarter(currentQuarter) {
 
   var currentQuarterValue = quarters[currentQuarter];
   console.log("Updating current quarter to " + currentQuarterValue);
-  var reportingPeriodLabel = $formJquery('label:contains("Reporting Period:")');
-  var reportingPeriodSelect = reportingPeriodLabel
-        .parents(".ACA_Form")
-        .find("select");
-  reportingPeriodSelect.val(currentQuarterValue);
-  if (reportingPeriodSelect.val() === quarters[currentQuarter]) {
-    console.log(`✅ listing id set to ${currentQuarter}`);
+
+  // Find the label element
+  var labels = specificFrame.querySelectorAll("label");
+  var reportingPeriodLabel = null;
+  labels.forEach(function (label) {
+    if (label.textContent.includes("Reporting Period:")) {
+      reportingPeriodLabel = label;
+    }
+  });
+
+  if (reportingPeriodLabel) {
+    // Find the select element
+    var reportingPeriodSelect = reportingPeriodLabel
+      .closest(".ACA_Form")
+      .querySelector("select");
+
+    if (reportingPeriodSelect) {
+      // Set the value of the select element
+      reportingPeriodSelect.value = currentQuarterValue;
+
+      if (reportingPeriodSelect.value === quarters[currentQuarter]) {
+        console.log(`✅ Current quarter set to ${currentQuarter}`);
+      } else {
+        console.log(
+          `❌ Current quarter is not set correctly ${reportingPeriodSelect.value}`
+        );
+      }
+    } else {
+      console.log(`❌ Select element not found`);
+    }
   } else {
-    console.log(`❌ listing id is not set correctly ${reportingPeriodSelect.val()}`);
+    console.log(`❌ Label element not found`);
   }
 }
 
+// function submitForm() {
+//   var submitButton = $formJquery("#ctl00_phPopup_btnSubmit");
+//     submitButton.click(function (res) {
+//       // Check if the click was successful
+//       if (true) {
+//         console.log(`✅ forms submitted`);
+//       } else {
+//         console.log(`❌ form could not be submitted`);
+//       }
+//     });
+//     submitButton.trigger("click");
+// }
+
 function submitForm() {
-  var submitButton = $formJquery("#ctl00_phPopup_btnSubmit");
-//   submitButton.click(function (res) {
-//     // Check if the click was successful
-//     if (true) {
-//       console.log(`✅ forms submitted`);
-//     } else {
-//       console.log(`❌ form could not be submitted`);
-//     }
-//   });
-//   submitButton.trigger("click");
+  console.log(`Submitting form`);
+  
+  // Find the submit button by its ID
+  var submitButton = specificFrame.querySelector("#ctl00_phPopup_btnSubmit");
+
+  if (submitButton) {
+    // Create and dispatch a click event on the submit button
+    var clickEvent = new Event("click", {
+      bubbles: true,
+      cancelable: true,
+    });
+
+    submitButton.dispatchEvent(clickEvent);
+
+    console.log(`✅ Form submitted`);
+  } else {
+    console.log(`❌ Submit button not found`);
+  }
 }
+
 
 /* ============= Run the scripts ============ */
 // const currentQuarter = 3;
